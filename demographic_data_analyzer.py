d1eccae4aff99ed45e10a4338187bf18aee174fc
Sharul Shafie <sharul.ariffin13@gmail.com>
2024-02-20 14:27:49 +0000

    add function for q1, q2, q3 and add file testing

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.groupby(['race'])['race'].count().sort_values(ascending=False)
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df['age'].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = len(df.groupby(['education']).get_group('Bachelors')) / len(df) * 100
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
